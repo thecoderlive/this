aa9c2717c6432459e41b98141b18b1ad2a025bdb
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-20 10:13:28 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "d3c45533-7e8b-4f28-9962-b84b126933f1",
+    "home_feed": [
+        {
+            "id": "6629f080-c4a7-4e76-942f-9b1ac4dbcf90",
+            "artist_name": "test",
+            "home_carousel": [
+                {
+                    "id": "ef1c65f7-d2c9-4177-915e-46ade2e8c7fa",
+                    "art_photo": "https://images.unsplash.com/photo-1624260319377-4f9e926297e4?w=1080"
+                },
+                {
+                    "id": "5b0712d6-a4f6-400e-bca9-02c428ad7282",
+                    "art_photo": "https://images.unsplash.com/photo-1573120923395-155c87bf2c31?w=1080"
+                },
+                {
+                    "id": "ac2ac478-da71-4193-acbd-cff672fddcdb",
+                    "art_photo": "https://images.unsplash.com/photo-1579083390476-968b47fe96f2?w=1080"
+                },
+                {
+                    "id": "f1486ac3-1569-4c09-b7bb-d8e11fc7f147",
+                    "art_photo": "https://images.unsplash.com/photo-1510566255260-5fff11a0c999?w=1080"
+                },
+                {
+                    "id": "faf7f07d-7f67-4097-a3d3-344a20d77f09",
+                    "art_photo": "https://images.unsplash.com/photo-1598298230762-e4cbaf605f4a?w=1080"
+                }
+            ],
+            "art_caption": "send",
+            "art_description": "Write husband hand lose human cell though attack. Leader daughter itself always garden eye."
+        },
+        {
+            "id": "656fceb9-4fa5-4bcc-835e-07b9b1ea0a12",
+            "artist_name": "sport",
+            "home_carousel": [
+                {
+                    "id": "12d37346-627f-4cfd-863c-e096ff5521ef",
+                    "art_photo": "https://images.unsplash.com/photo-1649472070259-46146d7c01b0?w=1080"
+                },
+                {
+                    "id": "566d6771-57b4-4789-ae8e-f662dc823f46",
+                    "art_photo": "https://images.unsplash.com/photo-1605555579795-81a19ff0ad50?w=1080"
+                },
+                {
+                    "id": "6ed530c3-7a1c-4665-8452-60581c40b3ed",
+                    "art_photo": "https://images.unsplash.com/photo-1493693108824-89c9e9174340?w=1080"
+                },
+                {
+                    "id": "1e22f600-067f-490b-9264-c3f91a356638",
+                    "art_photo": "https://images.unsplash.com/photo-1633078607232-c0b3517a046f?w=1080"
+                },
+                {
+                    "id": "12ab7d33-21d2-4138-9d83-52c465b7c9fd",
+                    "art_photo": "https://images.unsplash.com/photo-1557515527-269958c615de?w=1080"
+                }
+            ],
+            "art_caption": "see",
+            "art_description": "All marriage special state field. Brother factor become whose town himself. Arm maintain recognize white."
+        },
+        {
+            "id": "4c9a927d-96e4-41a7-b830-c8f8c5c57054",
+            "artist_name": "sense",
+            "home_carousel": [
+                {
+                    "id": "97d7cb3e-5b17-4bee-b63c-08420fb3dfc9",
+                    "art_photo": "https://images.unsplash.com/photo-1507272931001-fc06c17e4f43?w=1080"
+                },
+                {
+                    "id": "7b086435-3d98-415c-8c13-4294392bd05c",
+                    "art_photo": "https://images.unsplash.com/photo-1482100199117-a4a38a64e7e3?w=1080"
+                },
+                {
+                    "id": "cff36a13-6ab7-41af-b981-e9ea8b82505e",
+                    "art_photo": "https://images.unsplash.com/photo-1530948511201-3386d4730d73?w=1080"
+                },
+                {
+                    "id": "67e17607-e2d6-483f-bd93-95e02a8fb7bc",
+                    "art_photo": "https://images.unsplash.com/photo-1629898569904-669319348211?w=1080"
+                },
+                {
+                    "id": "fffba2fc-6cec-4c6f-9ddf-896bd1274822",
+                    "art_photo": "https://images.unsplash.com/photo-1470116945706-e6bf5d5a53ca?w=1080"
+                }
+            ],
+            "art_caption": "travel",
+            "art_description": "Weight establish when week word and product. Event sign end against help community."
+        },
+        {
+            "id": "8176576c-04cf-4c00-968e-ba487491df2c",
+            "artist_name": "necessary",
+            "home_carousel": [
+                {
+                    "id": "8f7cfa6d-4f9c-42b5-a26e-18912868c03d",
+                    "art_photo": "https://images.unsplash.com/photo-1610926950521-ab7e8b45f377?w=1080"
+                },
+                {
+                    "id": "6915e86a-47db-4bec-9f62-1ad81ad19931",
+                    "art_photo": "https://images.unsplash.com/photo-1546248449-439a5dc035bc?w=1080"
+                },
+                {
+                    "id": "05b45d25-7c75-4251-8f71-64d179417c4d",
+                    "art_photo": "https://images.unsplash.com/photo-1628149183763-3c145ee4aa27?w=1080"
+                },
+                {
+                    "id": "60d47850-60e8-433e-b87a-76084e28062c",
+                    "art_photo": "https://images.unsplash.com/photo-1581578021450-fbd19fba0e63?w=1080"
+                },
+                {
+                    "id": "de3c0d83-c1bb-411c-a16e-1dc01fc3faf6",
+                    "art_photo": "https://images.unsplash.com/photo-1636116461481-4c3ec444c9f8?w=1080"
+                }
+            ],
+            "art_caption": "she",
+            "art_description": "Process small child final. Data they benefit. Card a trade where draw marriage."
+        },
+        {
+            "id": "d1f653ac-2218-424e-8e0d-1f2920309a70",
+            "artist_name": "officer",
+            "home_carousel": [
+                {
+                    "id": "7cbe1abf-6f21-4891-8eef-c26ba6fd2b10",
+                    "art_photo": "https://images.unsplash.com/photo-1567476783296-fff06012e17a?w=1080"
+                },
+                {
+                    "id": "ac00b4ca-3463-41c8-9886-b6f29dbf1cfe",
+                    "art_photo": "https://images.unsplash.com/photo-1558877398-b21afc88ca01?w=1080"
+                },
+                {
+                    "id": "9532fae4-4c07-4690-b746-6b558b3fb96b",
+                    "art_photo": "https://images.unsplash.com/photo-1569671313489-5659dd977ff7?w=1080"
+                },
+                {
+                    "id": "c1d96cee-49f5-49cc-b485-1f5b12a7f130",
+                    "art_photo": "https://images.unsplash.com/photo-1635354701147-3b8e8210c438?w=1080"
+                },
+                {
+                    "id": "4753e5a5-7a40-42b4-9f0d-88b37984655b",
+                    "art_photo": "https://images.unsplash.com/photo-1528803030572-cf341cadda3e?w=1080"
+                }
+            ],
+            "art_caption": "guy",
+            "art_description": "Camera we community organization we. Ready rather cost fear material. True sing learn detail medical."
+        }
+    ]
+}
